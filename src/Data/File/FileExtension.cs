3d13bf98bbefa7c0f55ba74aa11658160108af60
FakyZDev <74829542+FakyDev@users.noreply.github.com>
2021-12-30 02:26:11 +0400

    Added new extension.

--- src/Data/File/FileExtension.cs
+++ src/Data/File/FileExtension.cs
@@ -3,6 +3,8 @@ namespace EchoCapture.Data.File{
     /// <summary> Hold valid extension which the application can parse.</summary>
     public enum FileExtension{
         json = 0,
-        png
+        png,
+        txt,
+        log
     }
 }
