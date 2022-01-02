bb6e15210a7a88a15935ba1882cd49c6a1516c5c
FakyZDev <74829542+FakyDev@users.noreply.github.com>
2022-01-03 02:44:39 +0400

    Added jpg file extension.

--- src/Data/File/FileExtension.cs
+++ src/Data/File/FileExtension.cs
@@ -2,9 +2,20 @@ namespace EchoCapture.Data.File{
 
     /// <summary> Hold valid extension which the application can parse.</summary>
     public enum FileExtension{
+
+        /// <summary> The extension of json file.</summary>
         json = 0,
+
+        /// <summary> The extension of an image file.</summary>
         png,
+
+        /// <summary> The extension of an image file.</summary>
+        jpg,
+
+        /// <summary> The extension of a text file.</summary>
         txt,
+
+        /// <summary> The extension of a text file, which is used to store logs.</summary>
         log
     }
 }
