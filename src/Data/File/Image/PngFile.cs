424930dc4e4ac94e264c8ccbf1a2b6ca4dc362c5
FakyZDev <74829542+FakyDev@users.noreply.github.com>
2022-01-03 02:46:17 +0400

    Now it is derived from ImageFile which has all the methods.

---
+++ src/Data/File/Image/PngFile.cs
@@ -0,0 +1,10 @@
+namespace EchoCapture.Data.File.Image{
+
+    /// <summary> Allows you to perform asynchronous and synchronous file operation to PNG image file.</summary>
+    public sealed class PngFile : ImageFile{
+        
+        /// <param name="name">The name of the png image.</param>
+        /// <param name="path">The directory the png image is located in.</param>
+        public PngFile(string name, string path) : base(name, path, FileExtension.png){}
+    }
+}
