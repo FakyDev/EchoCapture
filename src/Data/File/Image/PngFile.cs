38d9a68d89a52c0217f971c55203a2e61c31a4b7
FakyZDev <74829542+FakyDev@users.noreply.github.com>
2022-05-19 21:16:50 +0400

    Updated method as old method are updated.

--- src/Data/File/Image/PngFile.cs
+++ src/Data/File/Image/PngFile.cs
@@ -1,3 +1,5 @@
+using System.Drawing;
+
 namespace EchoCapture.Data.File.Image{
 
     /// <summary> Allows you to perform asynchronous and synchronous file operation to PNG image file.</summary>
@@ -6,5 +8,10 @@ namespace EchoCapture.Data.File.Image{
         /// <param name="name">The name of the png image.</param>
         /// <param name="path">The directory the png image is located in.</param>
         public PngFile(string name, string path) : base(name, path, FileExtension.png){}
+
+        /// <inheritdoc cref="EchoCapture.Data.File.Image.ImageFile.Screenshot(FileExtension)"/>
+        public static Bitmap Screenshot(){
+            return ImageFile.Screenshot(FileExtension.png);
+        }
     }
 }
