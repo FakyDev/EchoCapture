b357cf47a17b9f2968b9bc9b978b05b846bf0ef9
FakyZDev <74829542+FakyDev@users.noreply.github.com>
2022-05-23 00:20:42 +0400

    Removed unnecessary method.

--- src/Data/File/Image/JpgFile.cs
+++ src/Data/File/Image/JpgFile.cs
@@ -2,7 +2,7 @@ namespace EchoCapture.Data.File.Image{
 
     /// <summary> Allows you to perform asynchronous and synchronous file operation to JPEG image file.</summary>
     public sealed class JpgFile : ImageFile{
-
+        
         /// <param name="name">The name of the jpeg image.</param>
         /// <param name="path">The directory the jpeg image is located in.</param>
         public JpgFile(string name, string path) : base(name, path, FileExtension.jpg){}
--- src/Data/File/Image/PngFile.cs
+++ src/Data/File/Image/PngFile.cs
@@ -1,5 +1,3 @@
-using System.Drawing;
-
 namespace EchoCapture.Data.File.Image{
 
     /// <summary> Allows you to perform asynchronous and synchronous file operation to PNG image file.</summary>
@@ -9,9 +7,5 @@ namespace EchoCapture.Data.File.Image{
         /// <param name="path">The directory the png image is located in.</param>
         public PngFile(string name, string path) : base(name, path, FileExtension.png){}
 
-        /// <inheritdoc cref="EchoCapture.Data.File.Image.ImageFile.Screenshot(FileExtension)"/>
-        public static Bitmap Screenshot(){
-            return ImageFile.Screenshot(FileExtension.png);
-        }
     }
 }
