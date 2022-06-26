26eb56d7eb015c4e6463385f024d037cf3e6a986
FakyZDev <74829542+FakyDev@users.noreply.github.com>
2022-06-26 15:17:26 +0400

    Removed part of the struct.
    A part of the struct has been removed as its use has been moved to image setting.

--- src/Data/UpdateData.cs
+++ src/Data/UpdateData.cs
@@ -1,8 +1,6 @@
-using EchoCapture.Data.File;
-
 namespace EchoCapture.Data{
     
-    /// <summary> Struct used for updating data in application data file.</summary>
+    /// <summary> Struct used for updating setting in application data file.</summary>
     public struct UpdateData{
 
         /// <summary> Will hold the data type.</summary>
@@ -13,9 +11,6 @@ namespace EchoCapture.Data{
 
         /// <summary> Will hold the timeout values.</summary>
         private int? timeout;
-
-        /// <summary> Will hold the file extension of the capture screen.</summary>
-        private FileExtension? imageExtension;
 
         /// <summary> (Get only) Return the update type.</summary>
         public DataType UpdateType{
@@ -40,20 +35,12 @@ namespace EchoCapture.Data{
             }
         }
 
-        /// <summary> (Get only) Return the new image extension.</summary>
-        public FileExtension? ImageExtension{
-            get{
-                return this.imageExtension;
-            }
-        }
-
         /// <summary> For updating path, in data file.</summary>
         /// <param name="newPath"> The new path.</param>
         public UpdateData(string newPath){
             this.updateType = DataType.path;
             this.path = newPath;
             this.timeout = null;
-            this.imageExtension = null;
         }
 
         /// <summary> For updating timeout, in data file.</summary>
@@ -62,16 +49,6 @@ namespace EchoCapture.Data{
             this.updateType = DataType.timeout;
             this.timeout = newTimeout;
             this.path = null;
-            this.imageExtension = null;
-        }
-
-        /// <summary> For updating image file extension, in data file.</summary>
-        /// <param name="newExtension"> The new image file extension.</param>
-        public UpdateData(FileExtension newExtension){
-            this.updateType = DataType.imageFileExtension;
-            this.imageExtension = newExtension;
-            this.path = null;
-            this.timeout = null;
         }
 
         /// <summary> Define the update of application data file.</summary>
@@ -82,9 +59,6 @@ namespace EchoCapture.Data{
 
             /// <summary> Updates the time interval between screenshots.</summary>
             timeout,
-
-            /// <summary> Updates the image file extension.</summary>
-            imageFileExtension
         }
     }
 }
